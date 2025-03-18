ad4313c1a5203967b229c65f8c068e2813032343
Klevis Imeri <klevisimeri11@gmail.com>
2025-03-18 10:48:10 +0100

    Build

---
+++ dist/_app/immutable/entry/start.iFsNMzPD.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BnG8QpCv.js";export{o as load_css,r as start};
--- dist/_app/immutable/nodes/0.C-jbyUee.js
+++
@@ -1,1 +0,0 @@
-import{c as n,a}from"../chunks/CPyoKrdZ.js";import{o as p}from"../chunks/BdIUWPZI.js";import{s}from"../chunks/u22adTcs.js";const i=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,f as universal};
