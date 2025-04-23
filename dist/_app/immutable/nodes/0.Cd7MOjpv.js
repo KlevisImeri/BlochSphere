d847976b04afff6cad16edd9686a51e6f2e66751
Klevis Imeri <klevisimeri11@gmail.com>
2025-04-23 14:19:06 +0200

    Build

--- dist/_app/immutable/nodes/0.Cd7MOjpv.js
+++
@@ -1,1 +0,0 @@
-import{c as n,a}from"../chunks/BdRkxaRA.js";import{o as p}from"../chunks/WLnxfjTq.js";import{s}from"../chunks/C2tdxnXr.js";const i=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,f as universal};
